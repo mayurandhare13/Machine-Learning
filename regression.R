c2f5d438c12d01c1cb0b79ccf57b1cab2a15666b
MAYUR ANDHARE <mayurandhare13@gmail.com>
2018-12-29 15:59:49 -0800

    update Poly Regression

--- regression.R
+++ regression.R
@@ -10,7 +10,29 @@ y_pred = predict(sv_regressor, data.frame(Level = 6.5))
 
 ggplot() +
   geom_point(aes(x=dataset$Level, y=dataset$Salary), colors='red') +
-  geom_line(aes(x=dataset$Level, y=predict(sv_regressor, newdata=dataset)), colors='blue') +
+  geom_line(aes(x=dataset$Level, y=predict(sv_regressor, newdata=dataset)), colour='blue') +
   ggtitle('Truth or Bluff (SVR)') +
+  xlab("Levels") + 
+  ylab("Salary")
+
+# Polynomial Regression
+
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary~., data=dataset)
+
+y_pred = predict(poly_reg, newdata = data.frame(Level=6.5, Level2=6.5^2,
+                                                Level3=6.5^3, Level4=6.5^4))
+
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x=dataset$Level, y=dataset$Salary), colour='red') +
+  geom_line(aes(x=x_grid, y=predict(poly_reg, newdata=data.frame(Level=x_grid,
+                                                                        Level2=x_grid^2,
+                                                                        Level3=x_grid^3,
+                                                                        Level4=x_grid^4))), 
+            colour='blue') +
+  ggtitle("Truth or Bluff (Poly Regression)") +
   xlab("Levels") + 
   ylab("Salary")
