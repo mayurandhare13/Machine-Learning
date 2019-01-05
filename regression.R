8e51dcaefd6de1f927bb4db9d702bdf5f77ddd2a
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-01-05 00:52:28 -0800

    added Random Forest Regressor

--- regression.R
+++ regression.R
@@ -42,16 +42,38 @@ ggplot() +
 
   dt_regressor = rpart(formula=Salary~., data=dataset, 
                       control=rpart.control(minsplit=1))
-                      
+
   y_pred = predict(dt_regressor, data.frame(Level=6.5))
 
   x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
   
   ggplot() +
   geom_point(aes(x=dataset$Level, y=dataset$Salary), colour='red') +
-  geom_line(aes(x=dataset$Level, y=predict(dt_regressor, 
-                            newdata=dataset)), 
+  geom_line(aes(x=x_grid, y=predict(dt_regressor, 
+                            newdata=data.frame(Level=x_grid))), 
             colour='blue') +
   ggtitle("Truth or Bluff (Decision Tree Regression)") +
+  xlab("Levels") + 
+  ylab("Salary")
+
+
+# Random Forest Regressor
+
+  install.packages('randomForest')
+  library(randomForest)
+
+  rf_regressor = randomForest(x=dataset[1],
+                              y=dataset$Salary, ntree=200)
+
+  y_pred = predict(rf_regressor, data.frame(Level=6.5))
+
+  x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+  
+  ggplot() +
+  geom_point(aes(x=dataset$Level, y=dataset$Salary), colour='red') +
+  geom_line(aes(x=x_grid, y=predict(rf_regressor, 
+                            newdata=data.frame(Level=x_grid))), 
+            colour='blue') +
+  ggtitle("Truth or Bluff (Random Forest Regression)") +
   xlab("Levels") + 
   ylab("Salary")
