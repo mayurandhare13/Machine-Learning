6385f2a7f223b46200179764bd0b3851f6ada115
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-01-09 18:34:06 -0800

    added DT and RF

--- ML_algorithms.R
+++ ML_algorithms.R
@@ -5,7 +5,7 @@ library(caTools)
 
 set.seed(139)
 
-## Naive Bayes
+
 dataset = read.csv('data/Social_Network_Ads.csv')
 
 # encoding categorical features
@@ -20,11 +20,38 @@ train_data = subset(dataset, split == TRUE)
 test_data = subset(dataset, split == FALSE)
 
 # Scaling
+
 train_data[, 2:3] = scale(train_data[, 2:3])
 test_data[, 2:3] = scale(test_data[, 2:3])
 
+## Naive Bayes
 nb_classifier = naiveBayes(x = train_data[, -4], y = train_data$Purchased)
 
 y_pred = predict(nb_classifier, newdata=test_data[-4])
 
 cm = table(test_data[, 4], y_pred)
+
+## Decision Tree
+# we don't need to scale features as RF or DT are not based not euclidean distances
+# but based on conditions of independant variable
+
+library(rpart)
+
+dt_classifier = rpart(formula = Purchased ~ ., data=train_data)
+
+y_pred = predict(dt_classifier, newdata=test_data[-4], type='class')
+# type='class' will give you predicted value(here, 0/1). w/o it returns probability
+
+cm = table(test_data[, 4], y_pred)
+
+## Random Forest Classifier
+rf_classifier = randomForest(x=train_data[-4],
+                            y=train_data$Purchased, ntree=20)
+
+y_pred = predict(rf_classifier, newdata=test_data[-4])
+
+cm = table(test_data[, 4], y_pred)
+
+y_pred_2 = predict(rf_classifier, newdata = train_data[-4])
+
+cm = table(train_data[, 4], y_pred_2)
