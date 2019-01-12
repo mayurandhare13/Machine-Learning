e54d659dc69bdfb72b60afc400846c56e14fb0d7
MAYUR ANDHARE <43421646+mayurandhare13@users.noreply.github.com>
2019-01-11 23:14:19 -0800

    added Hierarchial Clustering

--- Clustering.R
+++ Clustering.R
@@ -20,3 +20,22 @@ clusplot(X, km$cluster, line = 0,
          plotchar = FALSE, span = TRUE,
          main = paste("Clusters of Client"), 
          xlab = "Annual Income", ylab = "Spending Score")
+
+## Hierarchial Clusterinig
+
+dendrogram = hclust(d = dist(X, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = paste("Dendrogram"),
+     xlab = "Customers", ylab = "Euclidean Distance")
+
+# Fitting Data
+hc = hclust(d = dist(X, method = 'euclidean'), method = 'ward.D')
+y_hc = cutree(hc, 5)
+
+# Cluster Visualization
+library(cluster)
+clusplot(X, y_hc, line = 0,
+         shade = TRUE, color = TRUE, labels = 2, 
+         plotchar = FALSE, span = TRUE,
+         main = paste("Clusters of Client"), 
+         xlab = "Annual Income", ylab = "Spending Score")
