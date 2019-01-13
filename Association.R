2395df36c3bd7d7d14b1addc77d1afbee6253609
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-01-12 19:17:30 -0800

    added Eclat Association

--- Association.R
+++ Association.R
@@ -1,6 +1,7 @@
-## APRIORI
-
 library(arules)
+
+## APRIORI
+# people bought this also bought that.
 
 # Dataset
 # dataset should be sparse matrix to operate
@@ -8,8 +9,17 @@ dataset <- read.transactions("data/Market_Basket_Optimisation.csv", sep = ",", r
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 
-# Train
+# Train apriori
 rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 
 # Visualization
 inspect(sort(rules, by = 'lift')[1:10])
+
+## ECLAT
+# items frequently bought together
+
+# Train eclat
+sets = eclat(data = dataset, parameter = list(support = 0.004, minlen = 2))
+
+# Visualization
+inspect(sort(sets, by = 'support')[1:10])
